a0081776cbe9c97728aa94a6abc998549014643e
Fabricio Vargas <78710496+Fvargas78@users.noreply.github.com>
2021-10-25 20:41:27 -0700

    Create js5part1.js

---
+++ js5part1.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  $("#myform").validate({
+
+		messages: {
+			fname: "Please enter your name",
+            gender: "Please choose your gender",
+            exercise: "how often do you exercise",
+            hobbies: "Please choose your favorite hobbie",
+	    },
+
+  /* the following is needed to place an error msg after the radio and checkbox elements */
+
+  errorPlacement: function(error, element) {
+  if ( element.is(":radio") || element.is(":checkbox")) {
+    error.appendTo( element.parent());
+  } else {
+    error.insertAfter(element.parent());
+  }
+  } // end errorPlacement
+ }); 
+});
